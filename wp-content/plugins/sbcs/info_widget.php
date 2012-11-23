aebec854edf88c6d8fa3c44e1e6569a7c98bd3d4
Jason Douglas <jdouglas71@gmail.com>
2012-11-23 14:18:47 -0800

    News & Information widget complete.

--- wp-content/plugins/sbcs/info_widget.php
+++ wp-content/plugins/sbcs/info_widget.php
@@ -13,80 +13,119 @@ class SBCS_Info_Widget
 	function widget($args, $instance)
 	{
 		extract($args,EXTR_SKIP);
+		wp_enqueue_style( "sbcs_widget_style", plugins_url('sbcs.css',__FILE__) );
+
 		$info_type =  ($instance['info_type'])?$instance['info_type']:"news_information";
 		echo $before_widget;
-
 		?>
-		<div class="sbcs-info-widget">
-		<?php echo $before_title; ?>
 		<?php
 		$count=0;
 		switch( $info_type )
 		{
 			case "news_information":
-				echo "News & Information";
+				echo $before_title;
 				echo $after_title;
 				?>
-				<hr class="sbcs_info">
-				<div class="sbcs_info">
-					<?php echo "News & Information"; ?><br />
-				</div>
+				<table class="sbcs_info_table" cellpadding="0" cellspacing="0">
+					<tr class="sbcs_info_table_row" style="background:blue;padding:0px;margin:0px;">
+						<td>
+							<img style="max-width:100%;width:100%;height:100%;padding:0px;margin:0px;" src='<?php echo site_url("/wp-content/plugins/sbcs/images/")."NewsTab.gif";?>'>
+						</td>
+					</tr>
+					<tr class="sbcs_info_table_row">
+						<td class="sbcs_info_table_col">
+							<b>Contact SBCS</b><br />
+							222 Dollison St.<br />
+							Eureka, CA 95501<br />
+							(707) 443-2735<br /><br />
+							<p><a href="http://www.stadiumroar.com/stbernardscatholicschool">Facilities Schedule</a></p>
+							<p><a href="https://sites.google.com/a/saintbernards.us/st-bernard-s-college-counseling/">College Counseling Department</a></p>
+							<p><a href='<?php echo site_url("/about-us/contact-us");?>'>Entire Directory</a></p>
+							<p><a href='<?php echo site_url("/pdfs/")."bell%20schedule.pdf";?>'>Bell Schedule</a></p>
+							<p>Email <a style="text-decoration: underline" href="mailto:pretzel@saintbernards.us?subject=Daily Bulletin Request">Lynn Pretzel</a> to receive our daily bulletin.</p>
+						</td>
+					</tr>
+				</table>
 	            <?php 
 				break;
 			case "crusader_nation":
+				?>
+				<div class="sbcs-info-widget">
+				<?php
+				echo $before_title;
 				echo "Crusader Nation";
 				echo $after_title;
 				?>
-				<hr class="sbcs_info">
-				<div class="sbcs_info">
-					<?php echo "Crusader Nation"; ?><br />
+					<hr class="sbcs_info">
+					<div class="sbcs_info">
+						<?php echo "Crusader Nation"; ?><br />
+					</div>
+				<div>
 	            <?php 
 				break;
 			case "uniforms":
+				?>
+				<div class="sbcs-info-widget">
+				<?php
+				echo $before_title;
 				echo "Uniforms";
 				echo $after_title;
 				?>
-				<hr class="sbcs_info">
-				<div class="sbcs_info">
-					<?php echo "Uniforms"; ?><br />
+					<hr class="sbcs_info">
+					<div class="sbcs_info">
+						<?php echo "Uniforms"; ?><br />
+					</div>
 				</div>
-	            <?php 
+				<?php 
 				break;
 			case "annual_report":
+				?>
+				<div class="sbcs-info-widget">
+				<?php
+				echo $before_title;
 				echo "Annual Report";
 				echo $after_title;
 				?>
-				<hr class="sbcs_info">
-				<div class="sbcs_info">
-					<?php echo "Annual Report"; ?><br />
+					<hr class="sbcs_info">
+					<div class="sbcs_info">
+						<?php echo "Annual Report"; ?><br />
+					</div>
 				</div>
 	            <?php 
 				break;
 			case "summer_reading_list":
+				?>
+				<div class="sbcs-info-widget">
+				<?php
+				echo $before_title;
 				echo "Summer Reading List";
 				echo $after_title;
 				?>
-				<hr class="sbcs_info">
-				<div class="sbcs_info">
-					<?php echo "Summer Reading List"; ?><br />
-				</div>
+					<hr class="sbcs_info">
+					<div class="sbcs_info">
+						<?php echo "Summer Reading List"; ?><br />
+					</div>
+                </div>
 	            <?php 
 				break;
 			case "mass_schedule":
+				?>
+				<div class="sbcs-info-widget">
+				<?php
+				echo $before_title;
 				echo "Mass Schedule";
 				echo $after_title;
 				?>
-				<hr class="sbcs_info">
-				<div class="sbcs_info">
-					<?php echo "Mass Schedule"; ?><br />
+					<hr class="sbcs_info">
+					<div class="sbcs_info">
+						<?php echo "Mass Schedule"; ?><br />
+					</div>
 				</div>
 	            <?php 
 				break;
 		}
 		?>
-		</div>
 		<?php
-		 echo $after_title;
 		 echo $after_widget;
 	}
 
