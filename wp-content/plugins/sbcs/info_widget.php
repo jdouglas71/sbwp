f2032a8c8e49df5ba6e4d9937722b177ed0466cd
Jason Douglas <jdouglas71@gmail.com>
2012-11-27 12:50:10 -0800

    Fleshing in the various info widgets.

--- wp-content/plugins/sbcs/info_widget.php
+++ wp-content/plugins/sbcs/info_widget.php
@@ -27,7 +27,7 @@ class SBCS_Info_Widget
 				echo $after_title;
 				?>
 				<table class="sbcs_info_table" cellpadding="0" cellspacing="0">
-					<tr class="sbcs_info_table_row" style="background:blue;padding:0px;margin:0px;">
+					<tr class="sbcs_info_table_row" style="padding:0px;margin:0px;">
 						<td>
 							<img style="max-width:100%;width:100%;height:100%;padding:0px;margin:0px;" src='<?php echo site_url("/wp-content/plugins/sbcs/images/")."NewsTab.gif";?>'>
 						</td>
@@ -38,8 +38,10 @@ class SBCS_Info_Widget
 							222 Dollison St.<br />
 							Eureka, CA 95501<br />
 							(707) 443-2735<br /><br />
+							<p><a href="http://sbcsbulletins.blogspot.com/">Bulletin</a></p>
 							<p><a href="http://www.stadiumroar.com/stbernardscatholicschool">Facilities Schedule</a></p>
 							<p><a href="https://sites.google.com/a/saintbernards.us/st-bernard-s-college-counseling/">College Counseling Department</a></p>
+							<p><a href="http://sbcscollegecounseling.blogspot.com/">Scholarships</a></p>
 							<p><a href='<?php echo site_url("/about-us/contact-us");?>'>Entire Directory</a></p>
 							<p><a href='<?php echo site_url("/pdfs/")."bell%20schedule.pdf";?>'>Bell Schedule</a></p>
 							<p>Email <a style="text-decoration: underline" href="mailto:pretzel@saintbernards.us?subject=Daily Bulletin Request">Lynn Pretzel</a> to receive our daily bulletin.</p>
@@ -80,47 +82,39 @@ class SBCS_Info_Widget
 				break;
 			case "annual_report":
 				?>
-				<div class="sbcs-info-widget">
 				<?php
 				echo $before_title;
-				echo "Annual Report";
 				echo $after_title;
 				?>
-					<hr class="sbcs_info">
-					<div class="sbcs_info">
-						<?php echo "Annual Report"; ?><br />
-					</div>
-				</div>
+					<a href="http://issuu.com/cvp-design/docs/sbcs-ar-2010?mode=embed&amp;layout=http%3A%2F%2Fskin.issuu.com%2Fv%2Fdark%2Flayout.xml&amp;showFlipBtn=true">
+						<img alt="SBCS AR 2010" height="195" longdesc="St. Bernard's Catholic School Annual Report 2010" src='<?php echo site_url("/wp-content/uploads/2012/11/")."ar10_web-iconsm.jpg";?>' width="150"><br>
+					</a>
 	            <?php 
 				break;
 			case "summer_reading_list":
 				?>
-				<div class="sbcs-info-widget">
 				<?php
 				echo $before_title;
-				echo "Summer Reading List";
 				echo $after_title;
 				?>
-					<hr class="sbcs_info">
-					<div class="sbcs_info">
-						<?php echo "Summer Reading List"; ?><br />
-					</div>
-                </div>
+					<p style="text-align:center;">
+						<big><b>Summer Reading List<br />1st Grade - 12th Grade</b></big><br />
+						<a href='<? echo site_url("/pdfs/")."Reading_list_2012.pdf";?>'>
+							(reading list)
+						</a>
+					</p>
 	            <?php 
 				break;
 			case "mass_schedule":
 				?>
-				<div class="sbcs-info-widget">
 				<?php
 				echo $before_title;
-				echo "Mass Schedule";
 				echo $after_title;
 				?>
-					<hr class="sbcs_info">
-					<div class="sbcs_info">
-						<?php echo "Mass Schedule"; ?><br />
-					</div>
-				</div>
+				<a href='<?php echo site_url("/pdfs/")."Mass_Schedule_2011-12.pdf";?>'>
+					<img src='<?php echo site_url("/wp-content/uploads/2012/11/")."Mass_Schedule.gif";?>' width="165" height="150">
+					<br />
+				</a>
 	            <?php 
 				break;
 		}
