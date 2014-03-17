b20ec1263f90ee5b41325e5b13db9ef5e5aa4ebc
Jason Douglas <jdouglas71@gmail.com>
2014-03-17 13:17:18 -0700

    Updated copyright date.

--- wp-content/themes/sbcs/footer.php
+++ wp-content/themes/sbcs/footer.php
@@ -28,7 +28,7 @@
 			<a href="http://www.douglasconsulting.net"> 
 				<img src="http://www.douglasconsulting.net/favicon.ico" style="width:32px;height:32px;vertical-align:middle;"> 
 			</a>Developed by <a href="http://www.douglasconsulting.net">Douglas Consulting</a>.<br /><br /> 
-			Copyright &copy; St. Bernard's Catholic School  2006-2012, All Rights Reserved
+			Copyright &copy; St. Bernard's Catholic School  2006-2014, All Rights Reserved
 		</div>
 		<div id="footer-bottom">
 		</div>
